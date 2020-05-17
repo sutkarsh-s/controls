53aed933c2822251325e3e2cb6f5b574049f49a3
sutkarsh-s <aarushsingh3006@gmail.com>
2020-05-17 18:55:43 +0530

    initial commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/utkarsh/catkin_ws/src
+# Build directory: /home/utkarsh/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_ros_demos/rrbot_control")
+subdirs("gazebo_ros_demos/rrbot_description")
+subdirs("gazebo_ros_demos/rrbot_gazebo")
+subdirs("beginner_tutorials")
+subdirs("gazebo_ros_demos/gazebo_tutorials")
+subdirs("testbot_description")
